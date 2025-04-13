b3e4598b823906d9f8b7e108494e7acf4519a18d
shahinalom <shahinalom3511371@gmail.com>
2025-04-13 23:21:50 +0600

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,21 +4,21 @@ import { BiLeaf } from "react-icons/bi";
 
 const Footer = () => {
       return (
-            <div className='bg- mt-10 text-gray-800 text-sm'>
+            <div className='bg-[#609513] mt-3 text-white text-sm'>
                   <Container>
-                        <div className='flex flex-col lg:flex-row justify-between items-center gap-2 py-4 text-center lg:text-left'>
-                              <h1 className='font-medium tracking-wide text-gray-800'>Assim Multilateral High School</h1>
+                        <div className='flex flex-col lg:flex-row justify-between items-center gap-2 py-6 text-center lg:text-left'>
+                              <h1 className='font-medium tracking-wide text-white'>Assim Multilateral High School</h1>
                               <h1 className='flex items-center gap-1 text-sm'>
-                                   Developed By
+                                    Developed By
                                     <span className='flex items-center gap-1 font-medium'>
-                                          <BiLeaf className='text-[#609513] text-lg' />
+                                          <BiLeaf className='text-white text-lg' />
                                           Shahin Alom
                                     </span>
                               </h1>
                         </div>
                   </Container>
-                  <h1 className='text-center text-sm py-4 px-4 border-t border-[#b4c4a4] bangla'>
-                         {new Date().getFullYear()} সমস্ত অধিকার সংরক্ষিত।
+                  <h1 className='lg:max-w-screen-xl mx-auto text-center text-sm py-4 px-4 border-t border-white bangla'>
+                        &copy; {new Date().getFullYear()} সমস্ত অধিকার সংরক্ষিত।
                   </h1>
             </div>
       );
