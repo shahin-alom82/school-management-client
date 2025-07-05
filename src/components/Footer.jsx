ea76b1896f994719c2a9bed066eaa721b4397514
shahinalom <shahinalom3511371@gmail.com>
2025-07-05 22:58:51 +0600

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { BiLeaf } from "react-icons/bi";
 const Footer = () => {
       return (
             <div className=' text-white text-sm mt-6'>
+                  {/* <h1></h1> */}
                   <Container className={'bg-[#609513] rounded-t-md'}>
                         <div className='flex flex-col lg:flex-row justify-between items-center gap-2 py-6 text-center lg:text-left lg:px-4'>
                               <h1 className='font-medium tracking-wide text-white'>Assim Multilateral High School</h1>
@@ -18,7 +19,7 @@ const Footer = () => {
                         </div>
                   </Container>
                   <Container>
-                        <p className='lg:text-center py-6 text-gray-900 px-4 bg-[#ebeff0] tracking-wide lg:text-sm text-xs'>&copy; {new Date().getFullYear()} <span className='text-[#23b792]'>Assim Multilateral High School</span> | Organized By <span className='text-[#23b792]'>SSC Batch - 2020</span> All Rights Reserved.</p>
+                        <p className='lg:text-center py-6 text-gray-900 px-4 bg-[#ebeff0] tracking-wide lg:text-sm text-xs'>&copy; {new Date().getFullYear()} <span className='text-[#23b792]'>Assim Multilateral High School</span> | Organized By <span className='text-[#23b792]'>SSC Batch - 2019-20</span> All Rights Reserved.</p>
                   </Container>
             </div>
       );
